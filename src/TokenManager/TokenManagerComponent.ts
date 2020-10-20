f626912a83aa59856baa000c685602b3a7a516a6
Seraj Vahdati <svahdati71@gmail.com>
2020-10-20 13:20:10 +0330

    feat(TokenManagerComponent.ts): use cookie instead of LocalStorage to save token

--- src/TokenManager/TokenManagerComponent.ts
+++ src/TokenManager/TokenManagerComponent.ts
@@ -1,36 +1,35 @@
-import {EventManager} from "azi-tools";
+import { EventManager } from "azi-tools";
+import cookie from "react-cookies";
 
 export type TokenManagerEventHandlerFunctionType = (token?: string) => void;
-export type EventTypes = 'change';
+export type EventTypes = "change";
 
-const tokenName = 'token';
+const tokenName = "token";
 
 export class TokenManagerComponent {
-    private _token ?: string | null;
-    events = new EventManager<TokenManagerEventHandlerFunctionType, EventTypes>();
+  private _token?: string | null;
+  events = new EventManager<TokenManagerEventHandlerFunctionType, EventTypes>();
 
+  get token() {
+    if (this._token === undefined)
+      try {
+        this._token = cookie.load(tokenName) || null;
+      } catch (e) {
+        console.error("token could not be read from Cookies");
+      }
 
-    get token() {
-        if (this._token === undefined)
-            try {
-                this._token = localStorage.getItem(tokenName) || null;
-            } catch (e) {
-                console.error('token could not be read from localStorage');
-            }
+    return this._token || null;
+  }
 
-        return this._token || null;
-    }
+  set token(newToken: string | null) {
+    if (newToken) cookie.save(tokenName, newToken, { path: "/" });
+    else cookie.remove(tokenName, { path: "/" });
 
-    set token(newToken: string | null) {
-        if (newToken)
-            localStorage.setItem(tokenName, newToken);
-        else localStorage.removeItem(tokenName);
+    this._token = newToken || null;
+    this.events.trigger("change", newToken);
+  }
 
-        this._token = newToken || null;
-        this.events.trigger('change', newToken);
-    }
-
-    removeToken() {
-        this.token = null;
-    }
+  removeToken() {
+    this.token = null;
+  }
 }
